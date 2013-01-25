e59ce877caed6b150c76673d6874d830101c01fd
Mat Sadler <mat@redstudio.co.uk>
2013-01-25 17:05:05 +0000

    added theme and first profile

---
+++ application/views/welcome_message.php
@@ -0,0 +1,186 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=100%; initial-scale=1; maximum-scale=1; minimum-scale=1; user-scalable=no;" />
+    <link rel="icon" href="images/favicon.png" type="image/png">
+    <link rel="shortcut icon" href="images/favicon.png" type="image/png" />
+    <title>Home Page</title>
+    <link href="css/bootstrap.css" type="text/css" rel="stylesheet"/>
+    <link href="css/style.css" type="text/css" rel="stylesheet"/>
+    <link href="css/prettyPhoto.css" type="text/css" rel="stylesheet"/>
+    <link rel="stylesheet" type="text/css" href="css/style_common.css" />
+    <link rel="stylesheet" type="text/css" href="css/style5.css" />
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+    <script type="text/javascript" src="js/jquery.quicksand.js"></script>
+    <script type="text/javascript" src="js/superfish.js"></script>
+    <script type="text/javascript" src="js/hoverIntent.js"></script>
+    <script type="text/javascript" src="js/jquery.hoverdir.js"></script>
+    <script type="text/javascript" src="js/jquery.flexslider.js"></script>
+    <script type="text/javascript" src="js/jflickrfeed.min.js"></script>
+    <script type="text/javascript" src="js/jquery.prettyPhoto.js"></script>
+    <script type="text/javascript" src="js/jquery.elastislide.js"></script>
+    <script type="text/javascript" src="js/jquery.tweet.js"></script>
+    <script type="text/javascript" src="js/smoothscroll.js"></script>
+    <script type="text/javascript" src="js/jquery.ui.totop.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+    <script type="text/javascript" src="js/ajax-mail.js"></script>
+    <script type="text/javascript" src="js/accordion.settings.js"></script>
+    <script type="text/javascript" src="js/jquery.cslider.js"></script>
+    <script type="text/javascript" src="js/modernizr.custom.28468.js"></script>
+
+
+
+    <!--Le Google Fonts-->
+    <link href='http://fonts.googleapis.com/css?family=Economica:700,400italic' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+
+<header id="header">
+    <div class="container">
+        <div class="row">
+            <div class="span3 logo">
+                <a href="./index.html"><img src="images/logo.png" alt="logo"/></a>
+            </div>
+            <nav id="menu" class="pull-right">
+                <ul>
+                    <li><a href="./index.html" >Home</a></li>
+                    <li><a href="./about.html" class="current">Page</a>
+                        <ul>
+                            <li><a href="./about.html">About Us</a></li>
+                            <li class="last"><a href="./services.html" >Our Services</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="./portfolio2.html" >Portfolio</a>
+                        <ul>
+                            <li><a href="./portfolio2.html">Portfolio 2 col</a>
+                                <ul>
+                                    <li><a href="./portfolio2.html">Portfolio 2 Columns</a></li>
+                                    <li class="last"><a href="./portfolio2ex.html">Portfolio 2 Columns Extended</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="./portfolio3.html">Portfolio 3 col</a>
+                                <ul>
+                                    <li><a href="./portfolio3.html">Portfolio 3 Columns</a></li>
+                                    <li class="last"><a href="./portfolio3ex.html">Portfolio 3 Columns Extended</a></li>
+                                </ul>
+                            </li>
+                            <li class="last"><a href="./portfolio4.html">Portfolio 4 col</a>
+                                <ul>
+                                    <li><a href="./portfolio4.html">Portfolio 4 Columns</a></li>
+                                    <li class="last"><a href="./portfolio4ex.html">Portfolio 4 Columns Extended</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="./blog.html">Blog</a>
+                        <ul>
+                            <li><a href="./blog.html">Blog</a></li>
+                            <li class="last"><a href="./blog-single.html">Blog Single</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="./contact.html" >Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+
+    </div>
+</header>
+
+<!--breadcrumbs -->
+<div class="container breadcrumbs">
+    <h1>Chris Dixon</h1>
+    
+</div>
+
+
+<!--container-->
+<section id="container">
+    <div class="container">
+        <div class="row latest-blog">
+            <div class="title-divider span12"><h3>Professional Profile</h3></div>
+            <hr>
+            <!--page-->
+            <div class="span4">
+                <div class="tagline">
+                    <h3>Skills</h3>
+                </div>
+              
+               Work - Solicitor<br/>
+               Position - Senior Partner<br/>
+               Business - <a href="http://www.ker.co.uk">Kenneth Elliott and Rowe</a><br/>
+               Industry - Law<br/>
+            </div>
+            <div class="span4">
+                <div class="tagline">
+                    <h3>Memberships &amp; Associations</h3>
+                </div>
+               
+                <ul class="list-1">
+                <li><a href="http://www.laworld.com">Laworld</a> founder member and former Chairman</li>
+                 <li>   Legal 500 – Top Tier Commercial Property Firm</li>
+<li>Member Society of Trust and Estate Practitioners (STEP) </li>
+<li>Member Legal Experts 2008</li>
+<li>Member  Labour Finance in Industry Group </li>
+<li>Bondholder West ham United plc</li>
+<li>Member Club Wembley</li>
+<li>CAA registered PPL(A)</li>
+                </ul>
+            </div>
+            <div class="span4">
+                <div class="tagline">
+                    <h3>Comments</h3>
+                </div>
+                <p>Solicitor practising in <a href="http://www.legal500.com/firms/1912-kenneth-elliott-rowe/offices/2111-romford">Legal 500</a>
+                	   recommended firm Kenneth Elliott and Rowe in Romford Essex.</p>
+                	   
+                	   <p>Excelent reputation for <a href="http://www.legal500.com/c/south-east/real-estate/commercial-property-essex#paragraph_18061">commercial property.</a></p>
+<p>Conference attendee Laworld international legal conferences: <br/>
+	
+	 	New Dehli, Cyprus, Amsterdam, Macau, Kuala Lumpur, Tehran, Manama
+	 	
+	 	</p>                
+
+            </div>
+        </div>
+
+        <hr />
+
+        <div class="row aligncenter">
+            <h2>A whitelist is a list or register of entities that, for one reason or another,<br/> are being provided a particular privilege, service, mobility, access or recognition.</h2>
+            <p>White List is a unique internet profiling service for business users. Membership is by recommendation only.</p>
+
+ 
+
+            <p>
+                <a class="btn btn-primary btn-large">Contact us for further details.</a> 
+            </p>
+        </div>
+
+        <hr />
+
+       
+
+
+
+
+<!--footer menu-->
+<section id="footer-menu">
+    <div class="container">
+        <div class="row">
+            
+            <p class="span12"><span class="pull-left">Copyright 2012 - All Rights Reserved</span></p>
+        </div>
+    </div>
+</section>
+</body>
+</html>
